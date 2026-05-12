5ab6bfea7f89159a34df8c20b50266c9a2c78dd4
github-action-benchmark <github@users.noreply.github.com>
2026-05-12 00:07:47 +0000

    add RV32 SoC Simulation Benchmark (Verilator vs JIT) (customBiggerIsBetter) benchmark result for 1d51cb0d20de0a7bc8a573f4b30bd975371ede4e

--- dev/rv32-bench/data.js
+++ dev/rv32-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778542820308,
+  "lastUpdate": 1778544467806,
   "repoUrl": "https://github.com/Verilean/sparkle",
   "entries": {
     "RV32 SoC Simulation Benchmark (Verilator vs JIT)": [
@@ -1539,6 +1539,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "JIT evalTick+6wires (10M cycles)",
             "value": 4244690,
+            "unit": "cycles/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "junjihashimoto@users.noreply.github.com",
+            "name": "junji hashimoto",
+            "username": "junjihashimoto"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1d51cb0d20de0a7bc8a573f4b30bd975371ede4e",
+          "message": "Merge pull request #32 from Verilean/feature/notebook\n\nFeature/notebook",
+          "timestamp": "2026-05-12T09:02:08+09:00",
+          "tree_id": "4984953fc994bbcd491ac3da12d9dbcc91774301",
+          "url": "https://github.com/Verilean/sparkle/commit/1d51cb0d20de0a7bc8a573f4b30bd975371ede4e"
+        },
+        "date": 1778544467486,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Verilator (10M cycles)",
+            "value": 3137539,
+            "unit": "cycles/sec"
+          },
+          {
+            "name": "JIT eval+tick (10M cycles)",
+            "value": 3201177,
+            "unit": "cycles/sec"
+          },
+          {
+            "name": "JIT evalTick fused (10M cycles)",
+            "value": 3641096,
+            "unit": "cycles/sec"
+          },
+          {
+            "name": "JIT evalTick+6wires (10M cycles)",
+            "value": 3424884,
             "unit": "cycles/sec"
           }
         ]
